774ef1c8439c33b5091a542c8a8952c6df955f77
Sourav Mandal <sourav2406@gmail.com>
2016-11-15 18:46:42 +0530

    4th commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -12,4 +13,3 @@ def hello_name(name):
 
 if __name__ == '__main__':
 	app.run()
-	
